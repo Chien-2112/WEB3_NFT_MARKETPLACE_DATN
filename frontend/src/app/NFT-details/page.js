3742e918cbf76e3a776a9f3ea11ad0863ef5f3e3
Chien-2112 <tranchien2121@gmail.com>
2025-10-25 15:42:59 +0700

    create NFT Details Page UI

---
+++ frontend/src/app/NFT-details/page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// INTERNAL IMPORT.
+import Style from "./nftDetails.module.css";
+import { Button, Category, Brand } from '../../../components/componentsindex';
+import NFTDetailsPage from '../../../NFTDetailsPage/NFTDetailsPage';
+
+const NFTDetails = () => {
+	return (
+		<div>
+			<NFTDetailsPage />
+			<Category />
+			<Brand />
+		</div>
+	)
+}
+
+export default NFTDetails;
